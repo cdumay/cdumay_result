e3d73c4a0455127adcebc773b260b55172407455
Cédric Dumay <cedric.dumay@gmail.com>
2019-01-14 12:41:52 +0100

    devel: trait method merge instead of Add trait

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,6 @@ extern crate cdumay_error;
 #[macro_use]
 extern crate serde_derive;
 
-use std::ops::Add;
-use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ExecResult {
@@ -19,7 +17,7 @@ pub struct ExecResult {
     stdout: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     stderr: Option<String>,
-    retval: HashMap<String, serde_value::Value>,
+    retval: std::collections::HashMap<String, serde_value::Value>,
 }
 
 pub trait ExecResultProperties {
@@ -31,8 +29,8 @@ pub trait ExecResultProperties {
     fn stdout_mut(&mut self) -> &mut Option<String>;
     fn stderr(&self) -> &Option<String>;
     fn stderr_mut(&mut self) -> &mut Option<String>;
-    fn retval(&self) -> &HashMap<String, serde_value::Value>;
-    fn retval_mut(&mut self) -> &mut HashMap<String, serde_value::Value>;
+    fn retval(&self) -> &std::collections::HashMap<String, serde_value::Value>;
+    fn retval_mut(&mut self) -> &mut std::collections::HashMap<String, serde_value::Value>;
 
     fn is_error(&self) -> bool { *self.retcode() != 0 }
     fn search_value(&self, key: &str, default: Option<serde_value::Value>) -> Option<serde_value::Value> {
@@ -41,6 +39,7 @@ pub trait ExecResultProperties {
             None => default
         }
     }
+    fn merge(r1: &Self, r2: &Self) -> Self;
 }
 
 impl Default for ExecResult {
@@ -50,7 +49,7 @@ impl Default for ExecResult {
             retcode: 0,
             stdout: None,
             stderr: None,
-            retval: HashMap::new(),
+            retval: std::collections::HashMap::new(),
         }
     }
 }
@@ -65,39 +64,32 @@ impl ExecResultProperties for ExecResult {
     fn stdout_mut(&mut self) -> &mut Option<String> { &mut self.stdout }
     fn stderr(&self) -> &Option<String> { &self.stderr }
     fn stderr_mut(&mut self) -> &mut Option<String> { &mut self.stderr }
-    fn retval(&self) -> &HashMap<String, serde_value::Value> { &self.retval }
-    fn retval_mut(&mut self) -> &mut HashMap<String, serde_value::Value> { &mut self.retval }
-}
+    fn retval(&self) -> &std::collections::HashMap<String, serde_value::Value> { &self.retval }
+    fn retval_mut(&mut self) -> &mut std::collections::HashMap<String, serde_value::Value> { &mut self.retval }
+    fn merge(r1: &ExecResult, r2: &ExecResult) -> ExecResult {
+        let mut res = Self::default();
 
-impl Add<ExecResult> for ExecResult {
-    type Output = ExecResult;
+        *res.stdout_mut() = match (r1.stdout(), r2.stdout()) {
+            (None, None) => None,
+            (Some(ref data), None) | (None, Some(ref data)) => Some(data.clone()),
+            (Some(ref data1), Some(ref data2)) => Some(format!("{}\n{}", data1, data2))
+        };
 
-    fn add(self, other: ExecResult) -> <Self as Add<ExecResult>>::Output {
-        let mut res = ExecResult::default();
-        let mut stdout = String::new();
-        if let Some(data) = self.stdout {
-            stdout.push_str(&data);
+        *res.stderr_mut() = match (r1.stderr(), r2.stderr()) {
+            (None, None) => None,
+            (Some(ref data), None) | (None, Some(ref data)) => Some(data.clone()),
+            (Some(ref data1), Some(ref data2)) => Some(format!("{}\n{}", data1, data2))
+        };
+
+        for attr in &[r1, r2] {
+            for (k, v) in attr.retval().iter() {
+                res.retval_mut().insert(k.clone(), v.clone());
+            }
         }
-        if let Some(data) = other.stdout {
-            stdout.push_str(&data);
-        }
-        *res.stdout_mut() = Some(stdout);
-
-        let mut stderr = String::new();
-        if let Some(data) = self.stderr {
-            stderr.push_str(&data);
-        }
-        if let Some(data) = other.stderr {
-            stderr.push_str(&data);
-        }
-        *res.stderr_mut() = Some(stderr);
-
-        res.retval_mut().extend(self.retval);
-        res.retval_mut().extend(other.retval);
-
-        *res.retcode_mut() = match self.retcode > other.retcode {
-            true => self.retcode,
-            false => other.retcode
+        
+        *res.retcode_mut() = match r1.retcode() > r2.retcode() {
+            true => *r1.retcode(),
+            false => *r2.retcode()
         };
         res
     }
